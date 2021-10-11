38ce2811a445b1d708389c5e4581f91b72077b02
Daniel Anton Mata <anton.mata@gmail.com>
2021-10-12 04:52:05 +0800

    Undid path alias

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, 'src'),
-    },
-  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
